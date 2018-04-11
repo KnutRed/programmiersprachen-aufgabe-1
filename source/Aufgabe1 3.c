dbf38fe964b20f77c831f81c4e5bd8068dd01761
KnutRothe <knutrothe1996@gmail.com>
2018-04-11 16:50:01 +0200

    test1

--- source/Aufgabe1 3.c
+++ source/Aufgabe1 3.c
@@ -3,5 +3,13 @@
 int main()
 {
   std::cout << "Hello, World!\n";
+
+  for (int zahl = 0; zahl < 1000; zahl++) {
+    std::cout << "zahl = " + std::to_string(zahl) << std::endl;
+    }
+    
+    std::cout << std::endl;
+
+
   return 0;
 }

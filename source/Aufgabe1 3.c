cde4a957fb3ffaada16de6ba52ac8190f7c249d8
Knut <38220896+KnutRothe@users.noreply.github.com>
2018-04-23 02:24:37 +0200

    Add files via upload
    aufgabe 1 und 3

--- source/Aufgabe1 3.c
+++ source/Aufgabe1 3.c
@@ -2,14 +2,28 @@
 
 int main()
 {
-  std::cout << "Hello, World!\n";
+  float hilfsCounter = 0.0f;
 
-  for (int zahl = 0; zahl < 1000; zahl++) {
-    std::cout << "zahl = " + std::to_string(zahl) << std::endl;
+
+  std::cout << "Kleinste Zahl die durch 1 bis 20 teilbar ist: \n";
+
+  for (float zahl = 0.0f; zahl < 500000000.0f && (hilfsCounter < 20); zahl++) {
+    hilfsCounter = 0;
+    for (float teiler = 1.0f; teiler <= 20.0f && ((int)hilfsCounter % (int)teiler == 0.0f); teiler++){
+      hilfsCounter++;
+
     }
-    
-    std::cout << std::endl;
+    if(hilfsCounter >= 20.0f){
+        std::cout << " Zahl = " << zahl << std::endl;
+          std::cout << std::endl;
+
+    }
+
+  }
+
+  
 
 
   return 0;
 }
+
---
+++ source/Aufgabe1 5.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main()
+{
+
+    int a; // Initialisierung
+    a = 3; // Deklarierung
+
+    
+
+
+  std::cout << "Hello, World!\n";
+  return 0;
+}
